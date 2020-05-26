1e4a2a0a1fdb7c041d5d553e736f5d2546828d56
THIAGO DE BONIS <thiagobonis@outlook.com>
2020-05-26 15:29:58 -0300

    Correction in Layout and News Features

--- src/app/shared/components/utils/username/username.component.ts
+++ src/app/shared/components/utils/username/username.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class UsernameComponent implements OnInit {
 	@Input() text: string;
 	@Input() padding: string;
+	@Input() fontWeight: string;
+	@Input() fontColor: string;
 
 	constructor() {}
 
