9c55d675f533815e475a8d0290e9368c9a4d7010
THIAGO SAUD <60164558+thiagosaud@users.noreply.github.com>
2023-01-04 15:47:15 -0300

    feat: 🎸 Created Logic to Render in Mobile [CHAT-MAIN-CONTAINER]

--- src/app/shared/components/containers/user-panel-container/user-panel-container.component.ts
+++ src/app/shared/components/containers/user-panel-container/user-panel-container.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
 	selector: 'app-user-panel-container',
 	template: `
 		<app-panel-container [class]="'border-solid-left-light'">
+			<app-icon *ngIf="!isUserMainPanel" (onclick)="closeChat.emit()" [name]="'ARROW_BACK'" [containerPadding]="'0 1.5rem 0 0'"></app-icon>
+
 			<app-avatar [src]="userAvatar" [alt]="userAvatarAlt"></app-avatar>
 
 			<app-username *ngIf="username" [hasFlex]="true" [class]="'margin-rl-1'" [value]="username"></app-username>
@@ -13,6 +15,7 @@ import { Component, OnInit, Input } from '@angular/core';
 	`,
 })
 export class UserPanelContainerComponent implements OnInit {
+	@Output() closeChat = new EventEmitter();
 	@Input() isUserMainPanel: boolean;
 	@Input() username?: string;
 	@Input() userAvatar: string;
